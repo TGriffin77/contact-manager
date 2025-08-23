d02913677dab7d86b1da4b79cb6cd4fe1ca276e0
Thomas <tjgriffin77@gmail.com>
2025-08-23 19:00:48 -0400

    base setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+
+    
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
