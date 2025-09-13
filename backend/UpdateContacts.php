1c552a8a4138abf82eb76e5204588b8832914dc5
vasuramji52 <vasupradha.ramji@gmail.com>
2025-09-13 01:55:42 -0400

    Initial commit on empty branch

---
+++ backend/UpdateContacts.php
@@ -0,0 +1,44 @@
+<!-- FirstName, LastName, Phone, Email, UserID -->
+
+<?php
+	$inData = getRequestInfo();
+	
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET firstName = ?, lastName = ?, phone = ?, email = ? WHERE userId = ?");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
